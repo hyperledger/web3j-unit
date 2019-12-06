bbfebdc84438e29c486ad122f87e8ecf4a8c5937
Puneetha <puneetha17@gmail.com>
2019-12-06 18:35:42 +0000

    Fix failing geth greeter test

--- src/test/resources/geth/compose_start.sh
+++ src/test/resources/geth/compose_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-geth --nousb init /geth/dev.json
+geth --nousb init /geth/test.json
 geth --nousb account import /geth/key.txt --password /geth/password.txt
 geth --nousb --rpc --rpcaddr=0.0.0.0 --allow-insecure-unlock --unlock 0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 --password /geth/password.txt --mine
