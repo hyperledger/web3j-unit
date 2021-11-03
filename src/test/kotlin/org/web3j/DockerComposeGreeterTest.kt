4ed109b4e7662632df122e335b957e2b83ef8091
Alex <alexandrosrousos@gmail.com>
2021-11-03 19:21:00 +0000

    Disabled all docker tests

--- src/test/kotlin/org/web3j/DockerComposeGreeterTest.kt
+++ src/test/kotlin/org/web3j/DockerComposeGreeterTest.kt
@@ -13,12 +13,13 @@
 package org.web3j
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.web3j.greeter.Greeter
 import org.web3j.protocol.Web3j
 import org.web3j.tx.TransactionManager
 import org.web3j.tx.gas.ContractGasProvider
-
+@Disabled
 @EVMComposeTest("src/test/resources/compose/geth.yml", "node1", 8545)
 class DockerComposeGreeterTest {
 
