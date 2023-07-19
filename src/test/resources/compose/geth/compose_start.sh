126a9199a0ff2cb97fb2e31c396f478140d93289
Ignace Loomans <ignace.loomans@gmail.com>
2023-07-19 13:41:24 +0100

    Updated Scripts & Ran Geth Test

--- src/test/resources/compose/geth/compose_start.sh
+++ src/test/resources/compose/geth/compose_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-geth --nousb init /geth/test.json
-geth --nousb account import /geth/key.txt --password /geth/password.txt
-geth --nousb --rpc --rpcaddr=0.0.0.0 --allow-insecure-unlock --unlock 0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 --password /geth/password.txt --mine
+geth init ./genesis.json
+geth account import --password password.txt key.txt
+geth --http --http.api "eth,net,txpool" --http.addr=0.0.0.0 --allow-insecure-unlock --unlock 0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 --password password.txt --mine --miner.etherbase 0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 --rpc.allow-unprotected-txs
