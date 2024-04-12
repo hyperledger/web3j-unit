9062cf781dbd15b74224656a9d0ae50715a1acf8
Nischal Sharma <nischal@web3labs.com>
2024-04-12 12:57:50 +0530

    master to main changes
    Signed-off-by: Nischal Sharma <nischal@web3labs.com>

--- src/test/java/org/web3j/greeter/Greeter.java
+++ src/test/java/org/web3j/greeter/Greeter.java
@@ -36,7 +36,7 @@ import org.web3j.tx.gas.ContractGasProvider;
  *
  * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
  * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the <a
- * href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ * href="https://github.com/web3j/web3j/tree/main/codegen">codegen module</a> to update.
  *
  * <p>Generated with web3j version 4.4.0.
  */
